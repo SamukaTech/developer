5cafb4b02626d67df632a3885a6dd52e6f695977
SamukaTech <95984928+SamukaTech@users.noreply.github.com>
2021-12-11 20:22:27 -0300

    commit

---
+++ login.js
@@ -0,0 +1,18 @@
+function validaCampo(){
+var x = document.forms["formu"]["usuario"].value;
+if(x == null || x == "") {
+alert ("Você precisa digitar o seu nome!");
+return false;
+}
+var y = document.forms["formu"]["senha"].value;
+if(y == null || y == "") {
+alert (x + ", você precisa digitar a sua senha!");
+return false;
+}
+var senha = document.getElementById("Senha").value;
+if ( senha != "boletim") {
+alert("A senha que você digitou está incorreta. Tente novamente " + x + "!");
+return false;
+}
+return true;
+}
