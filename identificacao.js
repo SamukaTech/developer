74797f9fa8af50331e8a6619424062e39e0893fc
SamukaTech <95984928+SamukaTech@users.noreply.github.com>
2022-01-21 15:11:41 -0300

    recuperação de senha atualizado

--- identificacao.js
+++ identificacao.js
@@ -1,8 +1,8 @@
-var nome = prompt("PROVE SE VOCÊ ESTÁ CADASTRADO NO SISTEMA\n 1- Qual seu primeiro nome?"); 
-var insti = prompt("PROVE SE VOCÊ ESTÁ CADASTRADO NO SISTEMA\n 2- Em qual instituição de ensino você estuda?");
+var nome = prompt("PROVE SE VOCÊ ESTÁ CADASTRADO NO SISTEMA\n 1- Qual o seu primeiro nome?"); 
+var email = prompt("PROVE SE VOCÊ ESTÁ CADASTRADO NO SISTEMA\n 2- Qual o seu e-mail?");
 var serie = prompt("PROVE SE VOCÊ ESTÁ CADASTRADO NO SISTEMA\n 3- Qual foi a série/filme que você assistiu ultimamente?");
-if (nome != "Samuel" || insti != "UESPI" || serie != "Elite") {
-alert("Acesso negado!\n Os dados informados não consta em nosso sistema.");
+if (nome != "Samuel" || email != "samuelnaslima.98@gmail.com" || serie != "Elite") {
+alert("Acesso negado!\n Verifique se você preencheu todos os campos corretamente.");
 }
 else {
 alert("Olá Samuel, sua senha é: boletim10");
