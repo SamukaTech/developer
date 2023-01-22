9aeb6ac267363164e3cc5213331400c53ce7d8a0
SamukaTech <95984928+SamukaTech@users.noreply.github.com>
2023-01-21 21:44:11 -0300

    a

--- login..js
+++ login..js
@@ -1,17 +1,14 @@
 function acesso(form){
-    if (form.usuario.value == "Samuel" && form.senha.value == "boletim10") {
-        location = "notasdasprovas.html"
-    }
-    else if (form.usuario.value == "samuel" && form.senha.value == "concurso23"){
+    if (form.usuario.value == "samuelnaslima.98@gmail.com" && form.senha.value == "concurso23") {
         location = "concursos.html"
     }
     else if (form.usuario.value == "") {
-        alert("Você precisa digitar o seu nome!")
+        alert("Por favor, informe o seu e-mail!")
     }
     else if (form.senha.value == "") {
-        alert("Você precisa digitar a sua senha!")
+        alert("Por favor, informe a sua senha!")
     }
     else {
-        alert("Ops! Não reconheci seu usuário e senha. Veja se está tudo certo e tente novamente.")
+        alert("Ops! Usuário não reconhecido. Veja se está tudo certo e tente novamente.")
     }
 }
