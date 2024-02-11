a6370a3ab64c785a75615241c474548fec50e08d
Samukatech <95984928+SamukaTech@users.noreply.github.com>
2024-02-11 20:42:40 -0300

    Update anotacoes.js

--- anotacoes.js
+++ anotacoes.js
@@ -119,7 +119,12 @@ function displayTasks() {
         displayTasks();
       }
     }
-
+ function clearTasks() {
+            const confirmacao = confirm("Tem certeza de que deseja apagar todas as anotações?");
+            if (confirmacao) {
+                document.getElementById('anotacoes').innerText = ""; // Apaga todas as anotações
+            }
+ }
     // Notificação quando as anotações são salvas
     document.getElementById('botsalva').addEventListener('click', function() {
       document.getElementById('meuPopup').style.display = 'block';
