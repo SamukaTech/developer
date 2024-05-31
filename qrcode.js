2ab6b5ca10fd3535826c4758d3519cd116e04d85
Samukatech <95984928+SamukaTech@users.noreply.github.com>
2024-05-31 20:07:47 -0300

    QRcode

--- qrcode.js
+++ qrcode.js
@@ -1,9 +1,7 @@
 const wrapper = document.querySelector(".wrapper"),
   qrInput = wrapper.querySelector(".form input"),
-  generateBtn = wrapper.querySelector("#generateBtn"),
-  qrImg = wrapper.querySelector("#qrCodeImg"),
-  qrCanvas = wrapper.querySelector("#qrCodeCanvas");
-
+  generateBtn = wrapper.querySelector(".form button"),
+  qrImg = wrapper.querySelector(".qr-code img");
 let preValue;
 
 generateBtn.addEventListener("click", () => {
@@ -15,21 +13,10 @@ generateBtn.addEventListener("click", () => {
   if (preValue === qrValue) return;
   preValue = qrValue;
   generateBtn.innerText = "Gerando QR Code...";
-
-  // Gerar QR Code usando API
   qrImg.src = `https://api.qrserver.com/v1/create-qr-code/?size=200x200&data=${qrValue}`;
   qrImg.addEventListener("load", () => {
     wrapper.classList.add("active");
     generateBtn.innerText = "Gerar QR Code";
-
-    // Desenhar QR Code no canvas
-    const ctx = qrCanvas.getContext("2d");
-    qrCanvas.width = qrImg.width;
-    qrCanvas.height = qrImg.height;
-    ctx.drawImage(qrImg, 0, 0);
-
-    // Exibir o canvas
-    qrCanvas.style.display = "block";
   });
 });
 
@@ -37,14 +24,16 @@ qrInput.addEventListener("keyup", () => {
   if (!qrInput.value.trim()) {
     wrapper.classList.remove("active");
     preValue = "";
-    qrCanvas.style.display = "none";
   }
 });
 
 qrImg.addEventListener("click", () => {
-  // Converter o canvas para um link de download
-  const link = document.createElement("a");
-  link.href = qrCanvas.toDataURL("image/png");
-  link.download = "qrcode.png";
-  link.click();
+  if (qrImg.src !== "") {
+    var link = document.createElement("a");
+    link.href = qrImg.src;
+    link.download = "qrcode.png";
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+  }
 });
