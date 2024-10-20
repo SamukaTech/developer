f16bfe6f27beece083bb8ff918d5e7ab94379bcc
Samuel Araújo Tecno Web <95984928+SamukaTech@users.noreply.github.com>
2024-10-19 22:37:29 -0300

    Update identificacao.js

--- identificacao.js
+++ identificacao.js
@@ -4,9 +4,9 @@ var cid = prompt("PROVE SE VOCÊ ESTÁ CADASTRADO NO SISTEMA\n 3- Em qual cidade
 var ano = prompt("PROVE SE VOCÊ ESTÁ CADASTRADO NO SISTEMA\n 4- Em que ano você nasceu?");
 if (tel == "994200398" && email == "samuelnaslima.98@gmail.com" && cid == "Barras" && ano == "1998" ||
 tel == "994200398" && email == "samuelnaslima.98@gmail.com" && cid == "BARRAS" && ano == "1998"){
-    alert("Olá Samuel, sua senha é: boletim10");
+    alert("Olá Samuel, Você está cadastrado em dois sistemas a senha de acesso ao site concursos públicos é: concursos24 e a senha de acesso as notas acadêmicas é: boletim10");
 }
-else if (tel == "994609245" && email == "vitoriasamea.10@gmail.com" && cid == "Teresina" && ano == "2011" ||
+else if (tel == "994609245" && email == "samuelnaslima.98@gmail.com" && cid == "Barras" && ano == "2011" ||
 tel == "994609245" && email == "vitoriasamea.10@gmail.com" && cid == "TERESINA" && ano == "2011") {
     alert("Olá Vitória, sua senha é: 2425");
 }
