ff464674feeb1a31d85b1f51b30ea4d97f74534b
Samuel Araújo Tecno Web <95984928+SamukaTech@users.noreply.github.com>
2024-12-30 22:03:17 -0300

    Update script.js

--- samflix/script.js
+++ samflix/script.js
@@ -1,4 +1,11 @@
 function toggleMenu() {
     const menu = document.querySelector('.menu');
+    const toggleButton = document.querySelector('.menu-toggle');
+
+    // Alterna a classe 'active' no menu
     menu.classList.toggle('active');
+
+    // Alterna o botão para indicar se o menu está aberto ou fechado
+    const isActive = menu.classList.contains('active');
+    toggleButton.textContent = isActive ? '✖' : '☰';
 }
