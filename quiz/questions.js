919703993b12e0602109c0d5c7a7b1135ee72af9
Samuel Araújo Tecno Web <95984928+SamukaTech@users.noreply.github.com>
2025-01-20 17:49:10 -0300

    Add files via upload

---
+++ quiz/questions.js
@@ -0,0 +1,68 @@
+let questions = [
+  {
+      numb: 1,
+      question: "O que são Recursos Naturais?",
+      answer: "Elementos da natureza que usamos no dia a dia.",
+      options: [
+          "Elementos que criamos em fábricas.",
+          "Elementos da natureza que usamos no dia a dia.",
+          "Produtos feitos apenas com tecnologia.",
+          "Apenas alimentos e água."
+      ]
+  },
+  {
+      numb: 2,
+      question: "Qual dos exemplos abaixo é um recurso natural renovável?",
+      answer: "Água",
+      options: [
+          "Petróleo",
+          "Carvão mineral",
+          "Água",
+          "Ouro"
+      ]
+  },
+  {
+      numb: 3,
+      question: "Por que precisamos cuidar dos recursos naturais não renováveis?",
+      answer: "Porque eles podem acabar e demorar milhões de anos para se formar.",
+      options: [
+          "Porque eles se renovam rapidamente.",
+          "Porque eles podem acabar e demorar milhões de anos para se formar.",
+          "Porque não são úteis para o nosso dia a dia.",
+          "Porque não são importantes para o meio ambiente."
+      ]
+  },
+  {
+      numb: 4,
+      question: "Qual das alternativas contém apenas recursos naturais não renováveis?",
+      answer: "Petróleo, carvão mineral e ouro.",
+      options: [
+          "Luz solar, vento e árvores.",
+          "Água, solo e animais.",
+          "Petróleo, carvão mineral e ouro.",
+          "Energia solar, água e gás natural."
+      ]
+  },
+  {
+      numb: 5,
+      question: "Qual ação ajuda a preservar os recursos naturais?",
+      answer: "Usar água e energia de forma consciente.",
+      options: [
+          "Usar água e energia de forma consciente.",
+          "Jogar lixo em qualquer lugar.",
+          "Consumir tudo sem pensar no futuro.",
+          "Desmatar florestas sem replantar árvores."
+      ]
+  },
+  {
+    numb: 6,
+    question: "As pedras preciosas, juntamente com as rochas, areia e carvão são recursos:",
+    answer: "Minerais.",
+    options: [
+        "Arenosos.",
+        "Hídricos.",
+        "Energéticos.",
+        "Minerais."
+    ]
+}
+];
